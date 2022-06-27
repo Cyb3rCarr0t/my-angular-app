6fe987154c7d6e47a4ce9bf457c47a7cc3a52c79
Carolina <carolina.mascitti@gmail.com>
2022-06-27 18:30:22 +0300

    Create event handling component

--- src/app/event-handling/event-handling.component.ts
+++ src/app/event-handling/event-handling.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-handling.component.css']
 })
 export class EventHandlingComponent implements OnInit {
-
+  message = "I'm a component!";
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  doSomething(message: string): void{
+    alert('The button says: ' + message);
+  }
+
+  doSomethingElse(): void{
+    alert('Goodbye!');
+  }
 }
